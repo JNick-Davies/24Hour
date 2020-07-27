9f1665e5305c57709658633d4b25ebe214b14ce7
BobTerwilliger <jnickd09@gmail.com>
2020-07-27 18:48:06 -0400

    Content in Models

--- 24Hours.Data/Post.cs
+++ 24Hours.Data/Post.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,9 @@ namespace _24Hours.Data
     {
         [Key]
         public int PostId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; }
+
         [Required]
         public string Title { get; set; }
         [Required]
--- 24Hours.Models/ReplyList.cs
+++ 24Hours.Models/ReplyList.cs
@@ -8,5 +8,6 @@ namespace _24Hours.Models
 {
     public class ReplyList
     {
+        public string Title { get; set; }
     }
 }
